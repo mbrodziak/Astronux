de746e091a796017926512e5238d8870fb791743
Mateusz Brodziak <Mateusz@Mateusz>
2021-02-16 00:43:17 +0100

    Add home component (empty) and solar system component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SolarSystemComponent } from './solar-system/solar-system.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'solar-system',
+    component: SolarSystemComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/solar-system/solar-system.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solar-system',
+  templateUrl: './solar-system.component.html',
+  styleUrls: ['./solar-system.component.scss']
+})
+export class SolarSystemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
