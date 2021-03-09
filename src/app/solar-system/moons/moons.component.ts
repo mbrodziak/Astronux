cd70340c138e6877124dc4515bb28a2fc84ecafe
Mateusz Brodziak <Mateusz@Mateusz>
2021-03-09 22:57:48 +0100

    Scroll to top when routing

--- src/app/solar-system/moons/moons.component.ts
+++ src/app/solar-system/moons/moons.component.ts
@@ -1,5 +1,6 @@
 import { DBService, Moon } from './../../db.service';
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-moons',
@@ -10,9 +11,16 @@ export class MoonsComponent implements OnInit {
 
   moons: Array<Moon>;
 
-  constructor(private dbService: DBService) { }
+  constructor(private dbService: DBService, private router: Router) { }
 
   ngOnInit(): void {
     this.moons = this.dbService.getMoons();
+
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      window.scrollTo(0, 0);
+    });
   }
 }
--- src/app/solar-system/planets/planets.component.ts
+++ src/app/solar-system/planets/planets.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DBService, Planet } from './../../db.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-planets',
@@ -10,9 +11,16 @@ export class PlanetsComponent implements OnInit {
 
   planets: Array<Planet>;
 
-  constructor(private dbService: DBService) { }
+  constructor(private dbService: DBService, private router: Router) { }
 
   ngOnInit(): void {
     this.planets = this.dbService.getPlanets();
+
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      window.scrollTo(0, 0);
+    });
   }
 }
